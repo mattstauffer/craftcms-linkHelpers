799efe3697e652d7746b9591a70dd8bf19612c52
Matt Stauffer <mstauffer@gmail.com>
2014-08-10 21:44:51 -0400

    Add, and document, auto link filters for email and url and functions for email and url.

--- README.md
+++ README.md
@@ -1,10 +1,70 @@
 # Twig LinkHelpers for [Craft CMS](http://buildwithcraft.com/)
 
-Add a series of Twig filters for CraftCMS to add link helpers for URLs and emails.
+Add a series of Twig filters and functions for CraftCMS to add link helpers for URLs and emails.
 
 ## Installation
 1. Move the `linkhelpers` directory into your `craft/plugins` directory.
 2. Go to Settings &gt; Plugins from your Craft control panel and enable the `linkHelpers` plugin
 
 ## Usage
-@todo
+This plugin adds several Twig filters and several Twig functions.
+
+### Filters
+#### Auto-link emails
+Searches a string of text for email addresses and turns them into links.
+
+```twig
+{{ contentString | auto_link_emails }}
+```
+Or: 
+```twig
+{% filter auto_link_emails %}
+    <p>Content here with an email address like email@email.com.</p>
+{% endfilter}
+```
+
+Outputs:
+```html
+<p>Content here with an email address like <a href="mailto:email@email.com">email@email.com</a>.</p>
+```
+
+#### Auto-link URLs
+Searches a string of text for URLs and turns into links.
+
+```twig
+{{ contentString | auto_link_urls }}
+```
+Or: 
+```twig
+{% filter auto_link_emails %}
+    <p>Content here with a URL like http://www.site.com/</p>
+{% endfilter}
+```
+
+Outputs:
+```html
+<p>Content here with a URL like <a href="http://www.site.com/">http://www.site.com/</a></p>
+```
+
+### Functions
+#### Make a link to an email address
+```twig
+{{ linkEmail('email@email.com') }}
+```
+
+Outputs:
+
+```html
+<a href="mailto:email@email.com">email@email.com</a>
+```
+
+#### Make a link to a URL
+```twig
+{{ linkUrl('http://www.site.com/') }}
+```
+
+Outputs:
+
+```html
+<a href="http://www.site.com/">http://www.site.com/</a>
+```
--- linkHelpers/twigextensions/LinkHelpersTwigExtension.php
+++ linkHelpers/twigextensions/LinkHelpersTwigExtension.php
@@ -3,18 +3,15 @@ namespace Craft;
 
 use Twig_Extension;
 use Twig_Filter_Method;
+use Twig_Markup;
+use Twig_Function_Method;
 
 class LinkHelpersTwigExtension extends \Twig_Extension
 {
 	/**
-	 * @var array
+	 * @var No friggin clue what this is
 	 */
-	protected $input = array();
-
-	/**
-	 * @var array
-	 */
-	protected $fields = array();
+	static $charset;
 
 	/**
 	 * Get name of the Twig extension
@@ -22,9 +19,9 @@ class LinkHelpersTwigExtension extends \Twig_Extension
 	 * @return string
 	 */
 	public function getName()
-    {
-        return 'LinkHelpers';
-    }
+	{
+		return 'LinkHelpers';
+	}
 
 	/**
 	 * Get a list of the Twig filters this extension is providing
@@ -32,57 +29,151 @@ class LinkHelpersTwigExtension extends \Twig_Extension
 	 * @return array
 	 */
 	public function getFilters()
-    {
-        return array(
+	{
+		return array(
 			'auto_link_urls' => new Twig_Filter_Method($this, 'justUrls'),
 			'auto_link_emails' => new Twig_Filter_Method($this, 'justEmails'),
-			'auto_link' => new Twig_Filter_Method($this, 'urlsAndEmails'),
-        );
-    }
+			// 'auto_link' => new Twig_Filter_Method($this, 'urlsAndEmails'),
+		);
+	}
+
+	/**
+	 * Get a list of the Twig functions this extension is providing
+	 * 
+	 * @return array
+	 */
+	public function getFunctions()
+	{
+		return array(
+			'linkEmail' => new Twig_Function_Method($this, 'linkEmail'),
+			'linkUrl' => new Twig_Function_Method($this, 'linkUrl'),
+		);
+	}
+
+	/**
+	 * Create an HTML link from an email address
+	 * 
+	 * @param  string      $email_address
+	 * @return Twig_Markup
+	 */
+	public function linkEmail($email_address)
+	{
+		// @todo: Verify?
+
+		$return = "<a href=\"mailto:$email_address\">$email_address</a>";
+
+		return $this->returnString($return);
+	}
+
+	/**
+	 * Create an HTML link from a URL
+	 * 
+	 * @param  string      $url
+	 * @return Twig_Markup
+	 */
+	public function linkUrl($url)
+	{
+		// @todo: Verify?
+
+		$return = "<a href=\"$url\">$url</a>";
+
+		return $this->returnString($return);
+	}
 
 	/**
 	 * Auto-link the Urls in a string
 	 *
 	 * @param string $str
-	 * @return string
+	 * @return Twig_Markup
 	 */
 	public function justUrls($str)
 	{
-		$charset = craft()->templates->getTwig()->getCharset();
+		$str = $this->pregUrls($str);
 
-		$str = 'LINKED DUDE ' . $str;
-
-		return new \Twig_Markup($str, $charset);
+		return $this->returnString($str);
 	}
 
 	/**
 	 * Auto-link the email addresses in a string
 	 *
-	 * @param string $input
-	 * @return string
+	 * @param string $str
+	 * @return Twig_Markup
 	 */
-	public function justEmails($input)
+	public function justEmails($str)
 	{
-		$charset = craft()->templates->getTwig()->getCharset();
+		$str = $this->pregEmails($str);
 
-		$str = 'LINKED DUDE ' . $str;
-
-		return new \Twig_Markup($str, $charset);
+		return $this->returnString($str);
 	}
 
 	/**
 	 * Auto-link all Urls and Email addresses in a string 
 	 *
-	 * @param string $input
-	 * @return string
+	 * @param string $str
+	 * @return Twig_Markup
 	 */
-	public function urlsAndEmails($input)
+	public function urlsAndEmails($str)
 	{
-		$charset = craft()->templates->getTwig()->getCharset();
+		throw new Exception('Haven\'t figured this one out yet.');
 
-		$str = 'LINKED DUDE ' . $str;
+		$str = $this->pregEmails($this->pregUrls($str));
 
-		return new \Twig_Markup($str, $charset);
+		return $this->returnString($str);
 	}
 
+	/**
+	 * Regex to find URLs and replace them with full HTML links
+	 *
+	 * @source http://stackoverflow.com/a/10769415/1052406
+	 * @param string $string
+	 * @return string 
+	 *
+	 */ 
+	protected function pregUrls($string)
+	{
+		// @todo: Evaluate http://stackoverflow.com/a/1945957/1052406
+		$regexp = "/(<a.*?>)?(https?)?(:\/\/)?(\w+\.)?(\w+)\.(\w+)(<\/a.*?>)?/i";
+		$anchorMarkup = "<a href=\"%s://%s\" target=\"_blank\" >%s</a>";
+
+		preg_match_all($regexp, $string, $matches, \PREG_SET_ORDER);
+
+		foreach ($matches as $match)
+		{
+			if (empty($match[1]) && empty($match[7]))
+			{
+				$http = $match[2]?$match[2] : 'http';
+				$replace = sprintf($anchorMarkup, $http, $match[0], $match[0]);
+				$string = str_replace($match[0], $replace, $string);
+			}
+		}
+
+		return $string;
+	}
+
+	/**
+	 * Regex to find email addresses and replace them with full HTML links
+	 *
+	 * @source http://stackoverflow.com/a/626760/1052406
+	 * @param string $string
+	 * @return string 
+	 *
+	 */ 
+	protected function pregEmails($string)
+	{
+		$regex = '/(\S+@\S+\.\S+)/';
+		$replace = '<a href="mailto:$1">$1</a>';
+
+		return preg_replace($regex, $replace, $string);
+	}
+
+	/**
+	 * Return string, safed
+	 * 
+	 * @param  string $string
+	 * @return Twig_Markup
+	 */
+	protected function returnString($string)
+	{
+		return new Twig_Markup($string, craft()->templates->getTwig()->getCharset());
+	}
 }
